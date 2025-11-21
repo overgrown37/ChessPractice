eef40e05f6cdf767b43533d9c64e0b733a5be9a9
overgrown37 <anqh37@gmail.com>
2025-11-21 22:10:49 +0900

    .

---
+++ Assets/Scripts/MoveRangeCalculate.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRangeCalculate : MonoBehaviour
+{
+    public List<Vector2Int> GetMovableTilesWithinManhattanDistance(int startX, int startY, int moveDistance)
+    {
+        List<Vector2Int> movableTiles = new List<Vector2Int>();
+
+        // 맨해튼 거리가 moveDistance 이하인 모든 타일을 검사
+        for (int x = 0; x < 8; x++)
+        {
+            for (int y = 0; y < 8; y++)
+            {
+                // 맨해튼 거리 계산: |x1 - x2| + |y1 - y2|
+                int manhattanDistance = Mathf.Abs(startX - x) + Mathf.Abs(startY - y);
+
+                // 맨해튼 거리가 이동력 이하이고, 보드 범위 내에 있는 경우
+                if (manhattanDistance <= moveDistance && manhattanDistance > 0)
+                {
+                    movableTiles.Add(new Vector2Int(x, y));
+                }
+            }
+        }
+
+        return movableTiles;
+    }
+}
